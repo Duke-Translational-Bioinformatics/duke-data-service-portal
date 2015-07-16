e8732108ddfe6229dd2b6c8d337d81b83ff2ec2c
Casey Choiniere <caseychoiniere@gmail.com>
2015-07-16 11:37:00 -0700

    Add fetch library

--- app/scripts/stores/itemStore.js
+++ app/scripts/stores/itemStore.js
@@ -5,10 +5,12 @@ var ItemStore = Reflux.createStore({
 
     init() {
         this.items = [];
+        this.showDetailItem = null;
 
         this.listenTo(ItemActions.loadItems, this.loadItems);
         this.listenTo(ItemActions.loadItemsSuccess, this.loadItemsSuccess);
         this.listenTo(ItemActions.loadItemsError, this.loadItemsError);
+        this.listenTo(ItemActions.showItemDetail, this.showItemDetail);
     },
 
     loadItems() {
@@ -27,10 +29,22 @@ var ItemStore = Reflux.createStore({
     },
 
     loadItemsError(error) {
+        let msg = error && error.message ? error.message : 'An error occurred.';
         this.trigger({
-            error: error,
+            error: msg,
             loading: false
         });
+    },
+
+    showItemDetail(item) {
+        if(this.showDetailItem === item){
+            console.log("Same item already detailed");
+        } else {
+            this.showDetailItem = item;
+            this.trigger({
+                showDetailItem: this.showDetailItem
+            });
+        }
     }
 
 });
