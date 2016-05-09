d213e3a89cfb683b79dfaaf3d18b090ae5eb771e
Casey Choiniere <caseychoiniere@gmail.com>
2016-05-09 16:06:22 -0700

    Clears local storage of redirect url in app.js

--- app/scripts/pages/login.jsx
+++ app/scripts/pages/login.jsx
@@ -62,7 +62,6 @@ class Login extends React.Component {
             if (localStorage.getItem('redirectTo') !== null) {
                 let redUrl = localStorage.getItem('redirectTo');
                 document.location.replace(redUrl);
-                localStorage.removeItem('redirectTo');
             } else {
                 this.props.appRouter.transitionTo('/home');
             }
