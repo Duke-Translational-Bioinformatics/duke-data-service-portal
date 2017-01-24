f60e2d2914fcefc8e2c0dc44217b5cfb4fb3f918
Casey Choiniere <caseychoiniere@gmail.com>
2017-01-23 17:02:02 -0800

    Revert "Switches portal to use OIT Open ID for authentication"

--- app/scripts/actions/mainActions.js
+++ app/scripts/actions/mainActions.js
@@ -1,16 +1,17 @@
 import Reflux from 'reflux';
 import appConfig from '../config';
-import ProjectActions from '../actions/projectActions';
-import {UrlGen, Path} from '../../util/urlEnum';
-import { checkStatus, getFetchParams } from '../../util/fetchUtil';
+import {UrlGen} from '../../util/urlEnum';
 
 var MainActions = Reflux.createActions([
-    'getApiToken',
-    'getApiTokenSuccess',
-    'getApiTokenError',
+    'authenticationServiceValidate',
+    'authenticationServiceValidateSuccess',
+    'authenticationServiceValidateError',
     'clearErrors',
     'displayErrorModals',
     'failedUpload',
+    'getDdsApiToken',
+    'getDdsApiTokenSuccess',
+    'getDdsApiTokenError',
     'setApiToken',
     'getCurrentUser',
     'getCurrentUserSuccess',
@@ -25,43 +26,63 @@ var MainActions = Reflux.createActions([
     'removeErrorModal'
 ]);
 
-MainActions.getApiToken.preEmit = (appConfig, accessToken) => {
-    fetch(appConfig.baseUrl+UrlGen.routes.apiPrefix+Path.ACCESS_TOKEN+accessToken+'&authentication_service_id='+appConfig.serviceId, {
+MainActions.authenticationServiceValidate.preEmit = (appConfig, accessToken) => {
+    fetch(UrlGen.routes.authServiceUri + '/api/v1/token_info?access_token=' + accessToken, {
         method: 'get',
         headers: {
             'Accept': 'application/json',
             'Content-Type': 'application/json'
         }
-    }).then(checkResponse).then((response) => {
+    }).then(function (response) {
         return response.json()
-    }).then((json) => {
-        if (json.api_token) {
-            MainActions.getApiTokenSuccess(json.api_token)
+    }).then(function (json) {
+        if (json.signed_info) {
+            MainActions.authenticationServiceValidateSuccess(json.signed_info)
         } else {
-            throw "An error has occurred while trying to authenticate";
+            throw "Error has occurred";
         }
-    }).catch((ex) => {
-        MainActions.getApiTokenError(ex);
-        ProjectActions.handleErrors(ex);
+    }).catch(function (ex) {
+        MainActions.authenticationServiceValidateError(ex)
+    });
+};
+
+MainActions.getDdsApiToken.preEmit = (appConfig, signedInfo) => {
+    fetch(UrlGen.routes.baseUrl + '/api/v1/user/api_token?access_token=' + signedInfo, {
+        method: 'get',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        }
+    }).then(function (response) {
+        return response.json()
+    }).then(function (json) {
+        if (json && json.api_token) {
+            MainActions.setApiToken(json.api_token);
+            MainActions.getDdsApiTokenSuccess(json.api_token);
+        } else {
+            throw error;
+        }
+    }).catch(function (ex) {
+        MainActions.getDdsApiTokenError(ex)
     });
 };
 
 MainActions.getCurrentUser.preEmit = () => {
-    fetch(UrlGen.routes.baseUrl+UrlGen.routes.apiPrefix+Path.CURRENT_USER,
-        getFetchParams('get', appConfig.apiToken))
-        .then(checkResponse).then((response) => {
+    fetch(UrlGen.routes.baseUrl + '/api/v1/current_user', {
+        method: 'get',
+        headers: {
+            'Authorization': appConfig.apiToken,
+            'Accept': 'application/json'
+        }
+    })
+        .then(function (response) {
             return response.json()
-        }).then((json) => {
-            MainActions.getCurrentUserSuccess(json);
+        }).then(function (json) {
+            MainActions.getCurrentUserSuccess(json)
         })
-        .catch((ex) => {
-            MainActions.getCurrentUserError(ex);
-            ProjectActions.handleErrors(ex);
+        .catch(function (ex) {
+            MainActions.getCurrentUserError(ex)
         });
 };
-
-function checkResponse(response) {
-    return checkStatus(response, MainActions);
-}
 
 export default MainActions;
