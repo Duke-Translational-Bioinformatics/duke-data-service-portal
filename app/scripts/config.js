f140bbf0e4576d996d43b6baa3cc183d6c1f18ee
Casey Choiniere <caseychoiniere@gmail.com>
2017-01-24 14:02:54 -0800

    Chnages config.js

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -1,8 +1,8 @@
 let appConfig = {
-    serviceId: DDS_PORTAL_CONFIG.serviceId,
+    serviceId: 'be33eb97-3bc8-4ce8-a109-c82aa1b32b23',
     baseUrl: DDS_PORTAL_CONFIG.baseUrl,
-    authServiceUri: DDS_PORTAL_CONFIG.authServiceUri,
-    authServiceName:  DDS_PORTAL_CONFIG.authServiceName,
+    authServiceUri: 'https://oauth.oit.duke.edu/oidc/authorize?response_type=token&client_id=dds_dev',
+    authServiceName:  'OIT OpenID',
     securityState: DDS_PORTAL_CONFIG.securityState,
     apiToken: null,
     isLoggedIn: null,
