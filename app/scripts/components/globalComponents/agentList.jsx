807ba014e9404086291865dd3a5a387ce2ac6bcf
Casey Choiniere <caseychoiniere@gmail.com>
2018-01-26 09:59:49 -0800

    Change api url format in software agent credentials window

--- app/scripts/components/globalComponents/agentList.jsx
+++ app/scripts/components/globalComponents/agentList.jsx
@@ -48,7 +48,7 @@ class AgentList extends React.Component {
             {!loading ? <TextField
                 textareaStyle={styles.textArea}
                 style={styles.keyModal}
-                defaultValue={JSON.stringify({agent_key: agentKey.key, user_key: userKey.key, api_token: agentApiToken.api_token, api_url: DDS_PORTAL_CONFIG.baseUrl}, null, 4)}
+                defaultValue={JSON.stringify({agent_key: agentKey.key, user_key: userKey.key, api_token: agentApiToken.api_token, url: DDS_PORTAL_CONFIG.baseUrl+'/api/v1'}, null, 4)}
                 floatingLabelText="Agent Credentials"
                 id="keyText"
                 ref={(input) => this.keyText = input}
